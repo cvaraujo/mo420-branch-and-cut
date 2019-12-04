be277cea778b329de0519fb7b7fce943963a11f8
pedroonop <olimpio98@gmail.com>
2019-12-04 16:33:34 -0300

    Compilar em cpp11

---
+++ CMakeFiles/MO420_Branch_and_Cut.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MO420_Branch_and_Cut.dir/main.cpp.o"
+  "CMakeFiles/MO420_Branch_and_Cut.dir/src/graph.cpp.o"
+  "CMakeFiles/MO420_Branch_and_Cut.dir/src/model.cpp.o"
+  "MO420_Branch_and_Cut.pdb"
+  "MO420_Branch_and_Cut"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MO420_Branch_and_Cut.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

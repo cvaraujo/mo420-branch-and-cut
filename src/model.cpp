5afbe292af4433ade1baa7befd3c067f59d9081b
cvaraujo <carlosvdaraujo@gmail.com>
2019-12-04 12:00:36 -0300

    MO420: Hybrid formulation

--- src/model.cpp
+++ src/model.cpp
@@ -25,7 +25,7 @@ void Model::initialize() {
             this->x[i] = IloNumVarArray(env, graph->n);
 
 
-        for (auto i : graph->vertices) {
+        for (int i = 0; i < graph->n; i++) {
             sprintf(name, "y%d", i);
             y[i] = IloNumVar(env, 0, 1, name);
             model.add(y[i]);
@@ -107,3 +107,121 @@ void Model::solve() {
     this->cplex.exportModel("model.lp");
     this->cplex.solve();
 }
+
+/*-------------------------------------------------------
+
+void Model::initializeHybrid() {
+    try {
+        char name[20];
+        this->model = IloModel(env);
+        this->cplex = IloCplex(model);
+
+        this->z = IloArray<IloNumVarArray>(env, graph->n);
+        this->y = IloNumVarArray(env, graph->n);
+
+        for (int i = 0; i < graph->n; i++)
+            this->x[i] = IloNumVarArray(env, graph->n);
+
+
+        for (int i = 0; i < graph->n; i++) {
+            sprintf(name, "y%d", i);
+            y[i] = IloNumVar(env, 0, 1, name);
+            model.add(y[i]);
+            model.add(IloConversion(env, y[i], ILOBOOL));
+            for (auto j : graph->incidenceMatrix[i]) {
+                sprintf(name, "z%d%d", i, j);
+                z[i][j] = IloNumVar(env, 0, 1, name);
+                model.add(z[i][j]);
+                model.add(IloConversion(env, x[i][j], ILOBOOL));
+            }
+        }
+    } catch (IloException &ex) {
+        cout << ex.getMessage() << endl;
+        exit(EXIT_FAILURE);
+    }
+
+}
+
+void Model::initModelHybrid() {
+    cout << "Begin the model creation" << endl;
+    cplex.setParam(IloCplex::Param::TimeLimit, 600);
+    cplex.setParam(IloCplex::TreLim, 7000);
+//    cplex.setOut(env.getNullStream());
+
+    int root = 0;
+
+    objectiveFunction();
+    edgesLimitConstraintHybrid();
+    outDegreeHybrid(root);
+    branchesHybrid(root);
+    branchesCorrelationHybrid(root);
+    cout << "All done!" << endl;
+}
+
+void Model::edgesLimitConstraintHybrid() {
+    IloExpr constraint(env);
+
+    for (int i = 0; i < graph->n; i++)
+        for (auto j : graph->incidenceMatrix[i])
+            constraint += z[i][j];
+
+    // Edges in a extreme vertex with degree one have to be one
+    for (int i = 0; i < graph->n; i++)
+        for (auto j : graph->incidenceMatrix[i])
+            if (int(graph->incidenceMatrix[j].size()) == 1)
+                model.add(z[i][j] == 1);
+
+    model.add(constraint == (graph->n - 1));
+}
+
+void Model::outDegreeHybrid(int root){
+    for (int v = 0; v < n; v++) {
+        if (v != root) {
+            IloExpr constraint(env);
+            for (int i = 0; i < n; i++){
+                for (auto j : graph->incidenceMatrix[i]) {
+                    if (j == v) constraint += z[i][v];
+                }
+            }
+            model.add(constraint == 1);
+        }
+    }
+}
+
+void Model::branchesHybrid(int root){
+    for (int v = 0; v < n; v++) {
+        if (v != root) {
+            IloExpr constraint(env);
+            for (auto u : graph->incidenceMatrix[v])
+                constraint += z[v][u];
+
+            model.add(constraint - 1 <= (int(incidenceMatrix[v].size()) - 2)*y[v]);
+        } else {
+            IloExpr constraint(env);
+            for (auto u : incidenceMatrix[root])
+                constraint += z[root][u];
+
+            model.add(constraint - 2 <= (int(incidenceMatrix[root].size()) - 2)*y[v]);
+        }
+    }
+}
+
+void Model::branchesCorrelationHybrid(int root){
+    for (int v = 0; v < n; v++) {
+        if (v != root) {
+            IloExpr constraint(env);
+            for (auto u : graph->incidenceMatrix[v])
+                constraint += z[v][u];
+
+            model.add(2*y[v] <= constraint);
+        } else {
+            IloExpr constraint(env);
+            for (auto u : incidenceMatrix[root])
+                constraint += z[root][u];
+
+            model.add(2*y[root] <= constraint-1);
+        }
+    }
+}
+
+-------------------------------------------------------*/
